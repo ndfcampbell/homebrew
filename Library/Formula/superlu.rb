b24292671d3eccb4af1daaaa9791a6db8832dfa0
Neill D.F. Campbell <neill.campbell@cantab.net>
2012-11-28 02:57:28 +0000

    Clean up for superlu

--- Library/Formula/superlu.rb
+++ Library/Formula/superlu.rb
@@ -1,15 +1,10 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Superlu < Formula
   homepage ''
   url 'http://crd-legacy.lbl.gov/~xiaoye/SuperLU/superlu_4.3.tar.gz'
   version '4.3'
   sha1 'd2863610d8c545d250ffd020b8e74dc667d7cbdd'
-
-  # depends_on 'cmake' => :build
 
   def patches
     DATA
@@ -18,24 +13,13 @@ class Superlu < Formula
   def install
     # ENV.j1  # if your formula's build system can't parallelize
 
-	#system "cat make.inc"
-
     system "make"
-    
-    #system "make install" # if this fails, try separate make/make install steps
     
 	system "cp lib/libsuperlu_4.3.a lib/libsuperlu.a"
 	system "cp TESTING/libtmglib.a lib/libtmglib.a"
     lib.install 'lib/libsuperlu.a'
     lib.install 'lib/libtmglib.a'
   end
-
-#  def test
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test superlu`.
-#    system "false"
-#  end
 end
 
 __END__
