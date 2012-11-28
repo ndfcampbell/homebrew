cd8c5823e2f4ab5a29d2533a103440bc3013c5b4
Neill D.F. Campbell <neill.campbell@cantab.net>
2012-11-28 01:47:29 +0000

    Added arpack and superlu for compiling graphite 2.

---
+++ Library/Formula/arpack-ng.rb
@@ -0,0 +1,44 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class ArpackNg < Formula
+  homepage 'http://forge.scilab.org/index.php/p/arpack-ng'
+  url 'http://forge.scilab.org/index.php/p/arpack-ng/downloads/get/arpack-ng_3.1.2.tar.gz'
+  version '3.1.2'
+  sha1 'f5453e2d576f131890ca023e1d853e18920f9c3c'
+
+  # depends_on 'cmake' => :build
+  depends_on 'open-mpi'
+  depends_on 'openblas'
+
+  def install
+    # ENV.j1  # if your formula's build system can't parallelize
+    
+    ENV.fortran
+
+    # Include MPIF77, as the arpack-ng build process doesn't autodetect properly
+    ENV['MPIF77'] = 'mpif77'
+    
+    configure_args = ["--disable-dependency-tracking", "--prefix=#{prefix}", "--enable-shared"]
+    configure_args << "--with-blas=openblas"
+    configure_args << "--with-lapack=openblas"
+
+    system "./configure", *configure_args
+
+    system "make install"
+
+#    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+#    system "make install" # if this fails, try separate make/make install steps
+  end
+
+#  def test
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test arpack-ng`.
+#    system "false"
+#  end
+end
---
+++ Library/Formula/superlu.rb
@@ -0,0 +1,101 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Superlu < Formula
+  homepage ''
+  url 'http://crd-legacy.lbl.gov/~xiaoye/SuperLU/superlu_4.3.tar.gz'
+  version '4.3'
+  sha1 'd2863610d8c545d250ffd020b8e74dc667d7cbdd'
+
+  # depends_on 'cmake' => :build
+
+  def patches
+    DATA
+  end
+
+  def install
+    # ENV.j1  # if your formula's build system can't parallelize
+
+	system "cat make.inc"
+
+    system "make"
+    
+    #system "make install" # if this fails, try separate make/make install steps
+    
+    lib.install 'lib/libsuperlu_4.3.a'
+  end
+
+#  def test
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test superlu`.
+#    system "false"
+#  end
+end
+
+__END__
+diff -rupN make.inc make.inc
+--- ./make.inc	2012-11-27 19:19:38.000000000 +0000
++++ ./make.inc	2012-11-28 01:43:18.000000000 +0000
+@@ -16,25 +16,20 @@
+ #
+ #  The machine (platform) identifier to append to the library names
+ #
+-PLAT = _linux
++PLAT = _mac_x
+ 
+ #
+ #  The name of the libraries to be created/linked to
+ #
+-SuperLUroot	= $(HOME)/Codes/SuperLU/SuperLU_4.3
++#SuperLUroot	= $(HOME)/Codes/SuperLU_4.3
++SuperLUroot	=	../
+ SUPERLULIB   	= $(SuperLUroot)/lib/libsuperlu_4.3.a
+-TMGLIB       	= libtmglib.a
+-
+-## BLASLIB   	= $(SuperLUroot)/lib/libblas.a
+ 
+ BLASDEF 	= -DUSE_VENDOR_BLAS
+-BLASLIB 	= -L$(HOME)/lib/GotoBLAS -lgoto
+-
+-## ATLAS BLAS causes single-precision to fail
+-#BLASLIB   	= -L/usr/lib/atlas -lblas
+-## This BLAS causes single-precision to fail the test in SuperLU
+-#BLASLIB 	= -L/usr/lib -lblas
++#BLASLIB   	= $(SuperLUroot)/lib/libblas.a
++BLASLIB   	= -lBLAS
+ 
++TMGLIB       	= libtmglib.a
+ LIBS		= $(SUPERLULIB) $(BLASLIB)
+ 
+ #
+@@ -46,20 +41,21 @@ ARCHFLAGS    = cr
+ RANLIB       = ranlib
+ 
+ CC           = gcc
+-CFLAGS       = -DPRNTlevel=0 -O3
++CFLAGS       = -O2 
+ NOOPTS       = 
+-FORTRAN	     = g77
+-FFLAGS       = -O2
++#FORTRAN	     = /Applications/Absoft/bin/f90
++FORTRAN	     = gfortran
++FFLAGS       = -O3 -cpu:g5 -YEXT_NAMES=LCS -s -YEXT_SFX=_
+ LOADER       = $(CC)
+ LOADOPTS     =
+ 
+ #
+ #  C preprocessor defs for compilation for the Fortran interface
+-#  (-DNoChange, -DAdd_, -DAdd__, or -DUpCase)
++#  (-DNoChange, -DAdd_, -DUpCase, or -DAdd__)
+ #
+ CDEFS        = -DAdd_
+ #
+ # The directory in which Matlab is installed
+ #
+-MATLAB	     = /usr/sww/matlab
++MATLAB	     = /Applications/Matlab/MATLAB_R2012a.app/bin/matlab
+ 
+
